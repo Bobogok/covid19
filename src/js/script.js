1714ac5411f4719b61728da52eaa45a4ede7f7ef
qurias <qurias_it@mail.ru>
2021-11-01 23:07:49 +0300

    dist complete

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,27 @@
 $('.about__slider-comments').slick({
   dots: true,
-  infinite: true,
-  speed: 700,
+  speed: 500,
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 3,
-  autoplay: true
+  swipe: true
+});
+
+$('.slick-prev').html(' ');
+$('.slick-next').html(' ');
+
+let tags = document.querySelectorAll('[role="tab"]');
+
+tags.forEach((elem) => {
+  elem.innerHTML = ' ';
+  elem.addEventListener('click', () => {
+    tags.forEach((elem) => {
+      elem.style.width = '8px';
+      elem.style.height = '8px';
+      elem.style.backgroundColor = '#006ff9';
+    });
+    elem.style.width = '10px';
+    elem.style.height = '10px';
+    elem.style.backgroundColor = 'white';
+  });
 });
